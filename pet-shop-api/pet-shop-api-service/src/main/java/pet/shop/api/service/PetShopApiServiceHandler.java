e42decf7804d7f7ac8f844a8a7fea5221d3d5f4b
test <test@Test.com>
2017-05-04 21:54:18 +0100

    Wired service layer into persistence service.

--- pet-shop-api/pet-shop-api-service/src/main/java/pet/shop/api/service/PetShopApiServiceHandler.java
+++ pet-shop-api/pet-shop-api-service/src/main/java/pet/shop/api/service/PetShopApiServiceHandler.java
@@ -3,14 +3,11 @@
  */
 package pet.shop.api.service;
 
-import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import pet.shop.api.domain.Pet;
-import pet.shop.api.domain.Species;
-import pet.shop.api.domain.Status;
 import pet.shop.api.persistence.service.PetPersistenceService;
 
 /**
@@ -39,16 +36,10 @@ public class PetShopApiServiceHandler implements PetShopApiService {
     @Override
     public List<Pet> retrieveAllPets() {
 
-        final Pet dog = new Pet.PetBuilder().name("Rob").age(5).species(Species.DOG).status(Status.ADOPTED).build();
-        final Pet cat = new Pet.PetBuilder().name("Socks")
-            .age(1)
-            .species(Species.CAT)
-            .status(Status.AVAILABLE_FOR_ADOPTION)
-            .build();
+        // Retrieve all pets from the persistence layer.
+        final List<Pet> petList = petPersistenceService.findAllPets();
 
-        petPersistenceService.savePet(dog);
-
-        return Arrays.asList(dog, cat);
+        return petList;
     }
 
 }
